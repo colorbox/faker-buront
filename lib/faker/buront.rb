77189687a5e1c674e36c2f8c3afd6b7864a99cb7
colorbox <colorbox222@gmail.com>
2018-02-27 21:20:54 +0900

    Add some methods for title and oneline saying

--- lib/faker/buront.rb
+++ lib/faker/buront.rb
@@ -4,19 +4,28 @@ require 'faker/buront/version'
 
 module Faker
   module Buront
-
-    def self.saying
-      random_title = titles.sample
-      saying_hash[random_title]
+    def self.saying_oneline
+      random_saying[:body].split("\n").sample
     end
 
-    def self.saying_hash
-      saying_file = "#{File.dirname(__FILE__)}/../../config/saying.yml"
-      YAML.load_file(saying_file).deep_symbolize_keys
+    def self.random_saying
+      random_title = titles.sample
+      saying(random_title)
+    end
+
+    def self.saying(title)
+      saying_hash[title]
     end
 
     def self.titles
       saying_hash.keys
+    end
+
+    private
+
+    def self.saying_hash
+      saying_file = "#{File.dirname(__FILE__)}/../../config/saying.yml"
+      YAML.load_file(saying_file).deep_symbolize_keys
     end
   end
 end
