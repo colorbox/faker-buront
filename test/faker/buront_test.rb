ab11fe7e22a6d8be327fe5bc43a092b6269745aa
colorbox <colorbox222@gmail.com>
2018-02-27 21:41:02 +0900

    Add test

--- test/faker/buront_test.rb
+++ test/faker/buront_test.rb
@@ -1,11 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class Faker::BurontTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Faker::Buront::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+  def test_saying_returns_string
+    Faker::Buront.keys.each do |key|
+      saying = Faker::Buront.saying(key)
+      assert_kind_of(Hash, saying)
+      assert_kind_of(String, saying[:title])
+      assert_kind_of(String, saying[:body])
+    end
   end
 end
