2c22a0d1dc0fad9c9595b6edca71bacce1fa3b0c
colorbox <colorbox222@gmail.com>
2018-02-27 21:53:27 +0900

    Add test for title and body

--- test/faker/buront_test.rb
+++ test/faker/buront_test.rb
@@ -5,8 +5,17 @@ class Faker::BurontTest < Minitest::Test
     Faker::Buront.keys.each do |key|
       saying = Faker::Buront.saying(key)
       assert_kind_of(Hash, saying)
-      assert_kind_of(String, saying[:title])
-      assert_kind_of(String, saying[:body])
+      assert_kind_of(String, Faker::Buront.saying_title(key))
+      assert_kind_of(String, Faker::Buront.saying_body(key))
     end
+  end
+
+  def test_with_ls_reliability
+    expect_title = 'ナイトと忍者のＬＳ信頼度は違いすぎた'
+    assert_equal(expect_title, Faker::Buront.saying_title(:ls_reliability))
+
+    expect_body_first_line = 'やはり忍者よりナイトの方が頼りにされていた'
+    actual_body_first_line = Faker::Buront.saying_body(:ls_reliability).split("\n").first
+    assert_equal(expect_body_first_line, actual_body_first_line)
   end
 end
