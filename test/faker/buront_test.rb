aa40572aacbf54efb2999af2845103522a8005f5
colorbox <colorbox222@gmail.com>
2018-02-28 23:25:52 +0900

    Add test for oneline body

--- test/faker/buront_test.rb
+++ test/faker/buront_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class Faker::BurontTest < Minitest::Test
   def test_saying_returns_string
     Faker::Buront.keys.each do |key|
-      saying = Faker::Buront.saying(key)
+      saying = Faker::Buront.whole_saying(key)
       assert_kind_of(Faker::Buront::Saying, saying)
       assert_kind_of(String, Faker::Buront.saying_title(key))
-      assert_kind_of(String, Faker::Buront.saying_body(key))
+      assert_kind_of(String, Faker::Buront.saying(key))
     end
   end
 
@@ -15,7 +15,14 @@ class Faker::BurontTest < Minitest::Test
     assert_equal(expect_title, Faker::Buront.saying_title(:ls_reliability))
 
     expect_body_first_line = 'やはり忍者よりナイトの方が頼りにされていた'
-    actual_body_first_line = Faker::Buront.saying_body(:ls_reliability).split("\n").first
+    actual_body_first_line = Faker::Buront.saying(:ls_reliability).split("\n").first
     assert_equal(expect_body_first_line, actual_body_first_line)
+  end
+
+  def test_with_fooly_silly
+    expected_oneline = 'またageてやがるメール欄に何か入れてればageを見破れないとでも思った浅はかさは愚かしい'
+    actual_oneline = Faker::Buront.whole_saying(:siily_fooly).oneline
+
+    assert_equal(expected_oneline, actual_oneline)
   end
 end
